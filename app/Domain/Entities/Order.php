8a4282d75d652ab77505c612185aaefe9a6c1912
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2019-12-19 22:07:22 -0500

    added Order entity;

---
+++ app/Domain/Entities/Order.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Invoicer\App\Domain\Entities;
+
+class Order extends AbstractEntity
+{
+    protected $customer;
+    protected $orderNumber;
+    protected $description;
+    protected $total;
+
+    public function getCustomer()
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(Customer $customer)
+    {
+        $this->customer = $customer;
+        return $this;
+    }
+
+    public function getOrderNumber()
+    {
+        return $this->orderNumber;
+    }
+
+    public function setOrderNumber($orderNumber)
+    {
+        $this->orderNumber = $orderNumber;
+        return $this;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    public function setTotal($total)
+    {
+        $this->total = $total;
+        return $this;
+    }
+}
