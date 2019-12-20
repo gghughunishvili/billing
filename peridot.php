f654ed1dad72738154f617bc6231f2cf293f91a4
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2019-12-20 00:07:08 -0500

    added peridot prophecy plugin to mock methods;

--- peridot.php
+++ peridot.php
@@ -6,5 +6,6 @@ use Peridot\Configuration;
 return function(EventEmitterInterface $emitter) {
     $emitter->on('peridot.configure', function(Configuration $config) {
         $config->setPath('specs');
+        new ProphecyPlugin($emitter);
     });
 };
