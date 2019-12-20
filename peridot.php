ff1f90baeedc169f56f4729988473a179b573def
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2019-12-19 23:25:04 -0500

    added basic peridot configuration;

---
+++ peridot.php
@@ -0,0 +1,10 @@
+<?php
+
+use Evenement\EventEmitterInterface;
+use Peridot\Configuration;
+
+return function(EventEmitterInterface $emitter) {
+    $emitter->on('peridot.configure', function(Configuration $config) {
+        $config->setPath('specs');
+    });
+};
