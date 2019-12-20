b537654b891bdd03eb5eae198bb9591bc1bef08b
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2019-12-19 23:44:57 -0500

    added tests that checks total of the invoices, associated order to the invoice and date setting for the invoice;

--- specs/domain/services/invoice-factory.spec.php
+++ specs/domain/services/invoice-factory.spec.php
@@ -15,4 +15,26 @@ describe('InvoiceFactory', function () {
             );
         });
     });
+
+    it('should set the total of the invoice', function () {
+        $order = new Order();
+        $order->setTotal(500);
+        $factory = new InvoiceFactory();
+        $invoice = $factory->createFromOrder($order);
+        expect($invoice->getTotal())->to->equal(500);
+    });
+
+    it('should associate the Order to the Invoice', function () {
+        $order = new Order();
+        $factory = new InvoiceFactory();
+        $invoice = $factory->createFromOrder($order);
+        expect($invoice->getOrder())->to->equal($order);
+    });
+
+    it('should set the date of the Invoice', function () {
+        $order = new Order();
+        $factory = new InvoiceFactory();
+        $invoice = $factory->createFromOrder($order);
+        expect($invoice->getInvoiceDate())->to->loosely->equal(new \DateTime());
+    });
 });
