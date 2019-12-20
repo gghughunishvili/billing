99dbe06c4fbf32a9ddc97babe9cdd2722ce9b502
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2019-12-20 00:01:36 -0500

    renamed factory directory in specs that was mixed up with services; added invoice-service basic test suit;

---
+++ specs/domain/services/invoice-service.spec.php
@@ -0,0 +1,8 @@
+<?php
+
+describe('InvoicingService', function () {
+    describe('->generateInvoices()', function () {
+        it('should query the repository for uninvoiced Orders');
+        it('should return an Invoice for each uninvoiced Order');
+    });
+});
