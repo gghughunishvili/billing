57d67cfd4ec5a60bff27e5ab5ae9e7d533717450
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2019-12-19 22:03:55 -0500

    added Customer entity;

---
+++ app/Domain/Entities/Customer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Invoicer\App\Domain\Entities;
+
+class Customer extends AbstractEntity
+{
+    protected $name;
+    protected $email;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+}
