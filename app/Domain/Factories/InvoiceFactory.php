3a0fecf95dcf4d9da463629284bad866d26c9b7c
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2019-12-19 23:37:50 -0500

    added createFromOrder function that returns invoice; added peridot leo package to support expect function;

--- app/Domain/Factories/InvoiceFactory.php
+++ app/Domain/Factories/InvoiceFactory.php
@@ -2,7 +2,12 @@
 
 namespace Invoicer\App\Domain\Factories;
 
+use Invoicer\App\Domain\Entities\Invoice;
+use Invoicer\App\Domain\Entities\Order;
+
 class InvoiceFactory
 {
-
+    public function createFromOrder(Order $order) {
+        return new Invoice();
+    }
 }
